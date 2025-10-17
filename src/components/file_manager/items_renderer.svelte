e15514c6791cee98b2f4de3d619a8642e796b351
keshav-writes-code <dhimankeshav201@gmail.com>
2025-10-18 01:49:39 +0530

    feat: enhancement to text color, overflow behaviour

--- src/components/file_manager/items_renderer.svelte
+++ src/components/file_manager/items_renderer.svelte
@@ -17,20 +17,20 @@
 {#if root_path && file_tree[0]}
   <ul
     class={isDirectChild(root_path, file_tree[0].path)
-      ? "menu bg-base-200 h-full menu-sm bg-base-200 rounded-box relative w-full select-none"
+      ? "menu bg-base-200 menu-sm bg-base-200 rounded-box relative w-full select-none flex-1 overflow-y-auto flex-nowrap text-[color-mix(in_srgb,var(--color-base-content)_70%,black)] text-ellipsis leading-relaxed tracking-wide"
       : ""}
   >
     {#each file_tree as node}
       {#if node.isDirectory}
         <li>
-          <details open>
-            <summary> {node.name} </summary>
+          <details class="w-full">
+            <summary class="hover:text-white"> {node.name} </summary>
             <ItemsRenderer file_tree={node.children} {root_path} />
           </details>
         </li>
       {:else}
         <li>
-          <a>{node.name} </a>
+          <a class="w-full hover:text-white truncate block">{node.name} </a>
         </li>
       {/if}
     {/each}
@@ -45,7 +45,8 @@
     content: "";
     width: 0.375rem;
     height: 0.375rem;
-    box-shadow: inset 2px 2px;
+    box-shadow: inset 2px 2px
+      color-mix(in srgb, var(--color-base-content) 40%, black);
     transform-origin: 50%;
     rotate: 135deg;
     transition-property: rotate;
