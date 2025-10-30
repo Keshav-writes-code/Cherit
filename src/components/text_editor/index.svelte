f23f2f7ffc6586d6db7e7f0b382a3ab9cd4ba983
keshav-writes-code <dhimankeshav201@gmail.com>
2025-10-31 01:21:34 +0530

    feat(editor): add text editing and file reading functionality to the text editor

--- src/components/text_editor/index.svelte
+++ src/components/text_editor/index.svelte
@@ -1,37 +1,25 @@
 <script lang="ts">
   import BreadCrumb from "@/components/breadcrumb_path/index.svelte";
+  import { readTextFile, BaseDirectory } from "@tauri-apps/plugin-fs";
   import type { FileNode } from "@/types";
-  import { add_class_to_hashtags } from "./key_event_handlers";
+  import TipTap from "@/components/text_editor/tiptap.svelte";
   let {
     filenode,
     root_path,
   }: { filenode: FileNode | undefined; root_path: string | undefined } =
     $props();
+  let text_content: string | undefined = $state();
 
-  let html: string | undefined = $state();
-  let last_pressed_key = "";
+  $effect(async () => {
+    if (!filenode) return;
+    text_content = await readTextFile(filenode.path);
+  });
 </script>
 
 <BreadCrumb {filenode} {root_path} />
-
-<div
-  bind:innerHTML={html}
-  contenteditable="true"
-  aria-label="Text editor"
-  role="textbox"
-  tabindex="0"
-  onkeydown={({ key }) => {
-    last_pressed_key = key;
-  }}
-  oninput={(e) => {
-    switch (last_pressed_key) {
-      case "#":
-        html = add_class_to_hashtags(e.currentTarget.innerHTML);
-        break;
-
-      default:
-        break;
-    }
-  }}
-  class="textarea w-full grow overflow-y-auto b-none outline-none shadow-none"
-></div>
+<div class="w-full px-8 flex justify-center flex-1 overflow-auto">
+  <div class="max-w-170 w-full prose-2xl prose-blue prose prose-invert">
+    <h2 class="w-full mb-16 mt-10">{filenode?.name}</h2>
+    <TipTap {text_content} />
+  </div>
+</div>
