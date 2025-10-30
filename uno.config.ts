141e46e8937bab316005541e0f97567d8279b595
keshav-writes-code <dhimankeshav201@gmail.com>
2025-10-31 01:21:34 +0530

    feat(editor): add text editing and file reading functionality to the text editor
    for the text editor: using
    [tiptap](https://tiptap.dev/docs/editor/getting-started/overview)
    
    which generates style less html
    then give styling with unocss typography preset

--- uno.config.ts
+++ uno.config.ts
@@ -3,12 +3,25 @@ import { transformerVariantGroup } from "unocss";
 import { presetIcons } from "unocss";
 import { presetMini } from "unocss";
 import { presetDaisyui } from "@0x-jerry/unocss-preset-daisyui";
+import { presetTypography } from "unocss";
 export default defineConfig({
   rules: [
     ["capitalize", { "text-transform": "capitalize" }],
     ["isolate", { isolation: "isolate" }],
   ],
-  presets: [presetMini(), presetIcons(), presetDaisyui()],
+  presets: [
+    presetMini(),
+    presetIcons(),
+    presetDaisyui(),
+    presetTypography({
+      cssExtend: {
+        h2: { "margin-top": 0, "margin-bottom": 0 },
+        li: { "margin-top": 0, "margin-bottom": 0 },
+        p: { "margin-top": 0, "margin-bottom": 0 },
+        ul: { "margin-top": 0, "margin-bottom": 0 },
+      },
+    }),
+  ],
   transformers: [transformerVariantGroup()],
   separators: [":"],
 });
