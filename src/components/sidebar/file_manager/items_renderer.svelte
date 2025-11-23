2af766ebc7b076be93977ee66308df381e934f01
keshav-writes-code <dhimankeshav201@gmail.com>
2025-11-24 03:05:24 +0530

    fix: ui scroll

--- src/components/sidebar/file_manager/items_renderer.svelte
+++ src/components/sidebar/file_manager/items_renderer.svelte
@@ -37,7 +37,7 @@
   <ul
     class="
     {is_root &&
-      'menu menu-sm rounded-box relative w-full select-none flex-1 overflow-y-auto flex-nowrap text-[color-mix(in_srgb,var(--color-base-content)_80%,black)] text-ellipsis leading-relaxed tracking-wide pb-50% '}
+      'menu menu-sm h-full rounded-box relative w-full select-none  overflow-y-auto flex-nowrap text-[color-mix(in_srgb,var(--color-base-content)_80%,black)] text-ellipsis leading-relaxed tracking-wide pb-50% '}
     flex before:content-none flex-col gap-0.5 pt-0.5"
   >
     {#each file_tree as node (node.path)}
