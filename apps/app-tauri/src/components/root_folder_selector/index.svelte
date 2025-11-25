b66281fc138856c5a8f94483dffd28c05df3f9ce
keshav-writes-code <dhimankeshav201@gmail.com>
2025-11-26 00:57:21 +0530

    feat: add mobile specific styles and stuff

--- apps/app-tauri/src/components/root_folder_selector/index.svelte
+++ apps/app-tauri/src/components/root_folder_selector/index.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-  import { root_folder_picker_dialog_state } from "@/misc_global_states.svelte";
+  import {
+    current_platform_type,
+    root_folder_picker_dialog_state,
+  } from "@/misc_global_states.svelte";
   import { open } from "@tauri-apps/plugin-dialog";
   import { LazyStore } from "@tauri-apps/plugin-store";
   import { onMount } from "svelte";
@@ -21,8 +24,13 @@
   open={root_folder_picker_dialog_state.open}
   class="modal z-11"
 >
-  <div class="modal-box p-0 size-80% max-w-none flex max-w-250">
-    <div class="w-70 bg-base-content/10">
+  <div
+    class="
+    {current_platform_type == 'desktop' && 'size-80% lt-sm:flex-col'}
+    {current_platform_type == 'mobile' && 'size-100% lt-sm:flex-col-reverse '}
+    modal-box p-0 max-w-none flex max-w-250"
+  >
+    <div class="min-w-70 bg-base-content/10">
       {#if recent_paths.length}
         <ul
           class="w-full bg-transparent gap-2 menu bg-base-200 rounded-box w-56"
@@ -56,7 +64,7 @@
             </li>
           {/each}
         </ul>
-      {:else}
+      {:else if current_platform_type == "desktop"}
         <div
           class="color-purple/60 i-tabler:folder-heart size-15 mx-auto mt-20"
         ></div>
@@ -65,7 +73,12 @@
         </p>
       {/if}
     </div>
-    <div class="grow px-10 pt-15 flex flex-col">
+    <div
+      class="
+      {current_platform_type == 'desktop' && 'px-10'}
+      {current_platform_type == 'mobile' && 'px-3'}
+      grow pt-14 flex flex-col"
+    >
       <div class="w-full flex flex-col items-center">
         <img alt="logo" class="size-30" src="logo_500.png" />
         <p class="font-[Recoleta] leading-normal mt-3 capitalize text-4xl">
@@ -74,29 +87,43 @@
         <p class="text-base-content/60">Version {__APP_VERSION__}</p>
       </div>
       <div class="w-full flex justify-between mt-20 b-b-neutral/30 pb-3 b-b-1">
-        <div>
-          <p class="leading-relaxed">Open Folder as Vault</p>
-          <p class="text-sm text-base-content/60">
-            Choose an existing folder for Markdown Files
-          </p>
-        </div>
-        <button
-          class="btn btn-primary w-30"
-          onclick={async () => {
-            const folder = await open({
-              multiple: false,
-              directory: true,
-              recursive: true,
-            });
-            if (!folder) return;
-            root_path = folder;
-            if (!recent_paths.includes(folder)) {
-              recent_paths = [folder, ...recent_paths].slice(0, 10);
-              await user_activity.set("recent_paths", recent_paths);
-              await user_activity.save();
-            }
-          }}>Open</button
-        >
+        {#if current_platform_type == "desktop"}
+          <div>
+            <p class="leading-relaxed">Open Folder as Vault</p>
+            <p class="text-sm text-base-content/60">
+              Choose an existing folder for Markdown Files
+            </p>
+          </div>
+          <button
+            class="btn btn-primary w-30"
+            onclick={async () => {
+              const folder = await open({
+                multiple: false,
+                directory: true,
+                recursive: true,
+              });
+              if (!folder) return;
+              root_path = folder;
+              if (!recent_paths.includes(folder)) {
+                recent_paths = [folder, ...recent_paths].slice(0, 10);
+                await user_activity.set("recent_paths", recent_paths);
+                await user_activity.save();
+              }
+            }}>Open</button
+          >
+        {:else if current_platform_type == "mobile"}
+          <ul class="menu menu-xl w-full rounded-box">
+            <li>
+              <button class="grid grid-cols-[auto_auto_1fr]">
+                <div class="size-6 i-tabler:folder-open"></div>
+                Open folder as vault
+                <div
+                  class="i-tabler:chevron-right size-6 justify-self-end"
+                ></div>
+              </button>
+            </li>
+          </ul>
+        {/if}
       </div>
     </div>
   </div>
