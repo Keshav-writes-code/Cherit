ba951b652e1fa3d421f09d3a0ee204a83a1b3289
keshav-writes-code <dhimankeshav201@gmail.com>
2025-12-23 13:25:02 +0530

    feat(menu): make it also take option position arguments

--- apps/app/src/lib/states/context_menu.svelte.ts
+++ apps/app/src/lib/states/context_menu.svelte.ts
@@ -8,12 +8,17 @@ class MenuState {
   on_close: (() => void) | null = null;
 
   // Open the menu
-  open(e: MouseEvent, items: MenuItem[]) {
+  open(e: MouseEvent, items: MenuItem[], pos?: { x: number; y: number }) {
     e.preventDefault(); // Stop browser context menu
     e.stopPropagation(); // Stop bubbling
+    if (pos) {
+      this.x = pos.x;
+      this.y = pos.y;
+    } else {
+      this.x = e.clientX;
+      this.y = e.clientY;
+    }
 
-    this.x = e.clientX;
-    this.y = e.clientY;
     this.items = items;
     this.visible = true;
   }
