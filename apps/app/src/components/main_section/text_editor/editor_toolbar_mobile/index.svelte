60cd3b7de81f3f68991c47441e8d9d600ac6acd7
keshav-writes-code <dhimankeshav201@gmail.com>
2026-01-17 11:17:10 +0530

    feat(app)[ui]: add better styling to the mobile editor toolbar

--- apps/app/src/components/main_section/text_editor/editor_toolbar_mobile/index.svelte
+++ apps/app/src/components/main_section/text_editor/editor_toolbar_mobile/index.svelte
@@ -17,7 +17,7 @@
 <div class="absolute bottom-2 w-full px-2 h-11">
   <ul
     transition:fade={{ duration: 100 }}
-    class="menu menu-horizontal overflow-x-auto flex-nowrap btn-soft bg-[var(--btn-bg)] color-[color-mix(in_srgb,var(--color-base-content)_70%,black)] rounded-box w-full h-full"
+    class="menu menu-horizontal overflow-x-auto flex-nowrap bg-[oklch(from_var(--color-base-100)_calc(l*1.2)_c_h)] b-y-1 b-t-[oklch(from_var(--color-base-100)_calc(l*1.4)_c_h)] b-b-[oklch(from_var(--color-base-100)_calc(l*0.9)_c_h)] shadow-md color-[color-mix(in_srgb,var(--color-base-content)_70%,black)] rounded-field w-full h-full p-0"
   >
     {#each toolbar_buttons as item}
       <li>
