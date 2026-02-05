77499e1cd53683ec9ea94b6f5c326cec59b92cbb
keshav-writes-code <dhimankeshav201@gmail.com>
2026-02-05 11:09:51 +0530

    fix(app)[ui]: show floating action button on mobile only

--- apps/app/src/components/main_section/text_editor/index.svelte
+++ apps/app/src/components/main_section/text_editor/index.svelte
@@ -93,7 +93,9 @@
     />
   </div>
 </div>
-<FloatingActionButton />
+{#if current_platform_type == 'mobile'}
+  <FloatingActionButton />
+{/if}
 
 {#if current_platform_type == 'mobile' && mobile_toolbar_visible && editor_view.data}
   <MobileToolbar editor_view={editor_view.data} />
