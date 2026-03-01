b82426f829fbd131ebeb9450ff94d1375f316df1
keshav-writes-code <dhimankeshav201@gmail.com>
2026-03-01 06:12:20 +0530

    refac(lm): update debugging logs and lm listner lifecycle

--- apps/app/src/components/general/app_settings/editor/lm_config/local/index.svelte
+++ apps/app/src/components/general/app_settings/editor/lm_config/local/index.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
   import { invoke } from '@tauri-apps/api/core';
   import { LLMStreamListener } from 'tauri-plugin-llm-api';
+  import { onMount } from 'svelte';
+
   const listener = new LLMStreamListener();
-  let text = $state<string>();
-  listener.setup({
-    onData: (id, data, timestamp) => {
-      text = new TextDecoder().decode(data);
-      console.log(text);
-    },
-    onError: (msg) => console.error('Error:', msg),
-    onEnd: (usage) => {
-      if (usage) {
-        console.log(`Done. ${usage.total_tokens} tokens used.`);
-      }
-    },
+  let text = $state<string>('hi');
+
+  onMount(async () => {
+    await listener.setup({
+      onData: (id, data, timestamp) => {
+        const chunk = new TextDecoder().decode(data);
+        text += chunk;
+        console.log('Incoming chunk:', chunk);
+      },
+      onError: (msg) => console.error('Error:', msg),
+      onEnd: (usage) => {
+        if (usage) {
+          console.log(`Done. ${usage.total_tokens} tokens used.`);
+        }
+      },
+    });
   });
 </script>
 
