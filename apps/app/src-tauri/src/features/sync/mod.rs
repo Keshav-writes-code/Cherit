51661af44e6a629e0f7bbe84802c269b0694c16e
keshav-writes-code <dhimankeshav201@gmail.com>
2026-03-21 22:03:13 +0530

    feat(app): add hashmap to manage the set of devices found

--- apps/app/src-tauri/src/features/sync/mod.rs
+++ apps/app/src-tauri/src/features/sync/mod.rs
@@ -1,9 +1,14 @@
-use local_ip_address::local_ip;
+use std::{
+    collections::HashSet,
+    net::{IpAddr, Ipv4Addr},
+};
+
+use local_ip_address::{list_afinet_netifas, local_ip};
 use mdns_sd::{ServiceDaemon, ServiceEvent, ServiceInfo};
 use serde::Serialize;
 use tauri::{Emitter, Window};
 
-#[derive(Clone, Serialize)]
+#[derive(Clone, Serialize, Hash, PartialEq, Eq)]
 struct DiscoveredDevice {
     name: String,
     ip: String,
@@ -22,14 +27,18 @@ fn gen_nick_name() -> String {
         NOUN.choose(&mut rng).unwrap()
     )
 }
-fn join_local_network(deamon: &ServiceDaemon, service_type: &str) {
+
+#[tauri::command]
+pub fn join_scan_local_network(win: Window) {
+    const SERVICE_TYPE: &str = "_cherit._udp.local.";
+    let deamon = ServiceDaemon::new().expect("Cannot create mdns deamon");
     let name = gen_nick_name();
     let active_ip = local_ip().expect("Failed to get local IP");
     let host_name = format!("{}.local.", active_ip);
     let properties = [("ip", active_ip.to_string()), ("name", name.to_string())];
 
     let service = ServiceInfo::new(
-        service_type,
+        SERVICE_TYPE,
         &name,
         &host_name,
         active_ip,
@@ -39,25 +48,22 @@ fn join_local_network(deamon: &ServiceDaemon, service_type: &str) {
     .expect("Cannot create ServiceInfo")
     .enable_addr_auto();
     deamon.register(service).expect("Cannot join service");
-}
-
-#[tauri::command]
-pub fn join_scan_local_network(win: Window) {
-    let mdns = ServiceDaemon::new().expect("Cannot create mdns deamon");
-    const SERVICE_TYPE: &str = "_cherit._udp.local.";
-    join_local_network(&mdns, SERVICE_TYPE);
 
     // Scan Local Network
     std::thread::spawn(move || {
-        let receiver = mdns.browse(SERVICE_TYPE).unwrap();
+        let receiver = deamon.browse(SERVICE_TYPE).unwrap();
+        let mut recevied_devices = HashSet::<DiscoveredDevice>::new();
 
         while let Ok(event) = receiver.recv() {
             if let ServiceEvent::ServiceResolved(info) = event {
-                let device = DiscoveredDevice {
-                    name: info.get_property_val_str("name").unwrap_or("").to_string(),
-                    ip: info.get_property_val_str("ip").unwrap_or("").to_string(),
-                };
-                let _ = win.emit("device_found", device);
+                if let Some(ip) = info.get_property_val_str("ip") {
+                    let device = DiscoveredDevice {
+                        name: info.get_property_val_str("name").unwrap_or("").to_string(),
+                        ip: ip.to_string(),
+                    };
+                    recevied_devices.insert(device);
+                    let _ = win.emit("device_found", &recevied_devices);
+                }
             }
         }
     });
