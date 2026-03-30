9e7d15184bf74b7c2f5fbe031c1aea5c7e7647ef
keshav-writes-code <dhimankeshav201@gmail.com>
2026-03-30 17:46:47 +0000

    refac: WIP add inital seperation logic leveragin bun workspaces, bun catalogs

--- apps/site-showcase/astro.config.mjs
+++ apps/site-showcase/astro.config.mjs
@@ -1,51 +1,14 @@
 import svelte from "@astrojs/svelte";
 import { defineConfig, fontProviders } from "astro/config";
 import UnoCSS from "unocss/astro";
-import mermaid from "astro-mermaid";
-
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
-import starlight from "@astrojs/starlight";
-import starlightSidebarTopics from "starlight-sidebar-topics";
 
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   prefetch: true,
-  integrations: [
-    UnoCSS(),
-    svelte(),
-    sitemap(),
-    robotsTxt(),
-    mermaid(),
-    starlight({
-      title: "Cherit",
-      plugins: [
-        starlightSidebarTopics([
-          {
-            id: "dev-docs",
-            label: "Dev Docs",
-            link: "docs/get-started",
-            icon: "laptop",
-            items: [
-              {
-                label: "Development",
-                autogenerate: { directory: "docs/dev_docs" },
-              },
-            ],
-          },
-          {
-            label: "Guides",
-            link: "docs/guides/how_to_setup_syncing",
-            icon: "rocket",
-            items: ["docs/guides/how_to_setup_syncing"],
-          },
-        ]),
-      ],
-    }),
-    mdx(),
-  ],
+  integrations: [UnoCSS(), svelte(), sitemap(), robotsTxt(), mdx()],
   site: "https://keshav.is-a.dev/",
   base: "Cherit",
   devToolbar: {
