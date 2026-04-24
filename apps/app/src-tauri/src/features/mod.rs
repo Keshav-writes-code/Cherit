596a3be85eda23a5fab2bc1f7c4f1d2188ca745b
keshav-writes-code <dhimankeshav201@gmail.com>
2026-04-24 22:21:17 +0530

    feat: add device info tauri plugin

--- apps/app/src-tauri/src/features/mod.rs
+++ apps/app/src-tauri/src/features/mod.rs
@@ -1,3 +1,3 @@
 pub mod file_system;
-pub mod sync;
 pub mod persistency;
+pub mod sync;
--- apps/app/src-tauri/src/lib.rs
+++ apps/app/src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ mod desktop_test;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_device_info::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_os::init())
