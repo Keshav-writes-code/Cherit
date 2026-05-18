62e42843d3a3af7189e7c3869d666ca17fb2baef
keshav-writes-code <dhimankeshav201@gmail.com>
2026-05-18 22:06:35 +0530

    refac: remove migration logic,use default derivation

--- apps/app/src-tauri/src/features/persistency/structs.rs
+++ apps/app/src-tauri/src/features/persistency/structs.rs
@@ -8,71 +8,46 @@ use std::{
 use tauri::{AppHandle, Manager};
 
 const CONFIG_FILE_NAME: &str = "config.json";
-const LATEST_SCHEMA_V: u8 = 1;
 
 // Global Struct
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Default)]
 struct GenericPath {
     path: PathBuf,
     document_top_tree_uri: Option<PathBuf>,
 }
 
 // Domain : Wokrspace
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Default)]
 struct WorkspaceMetaData {
     path: GenericPath,
     last_accessed: DateTime<Utc>,
     recent_filenode_path: Option<GenericPath>,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Default)]
 struct AppConfig {
     workspaces_metadata: Vec<WorkspaceMetaData>,
 }
 
-impl AppConfig {
-    pub fn new() -> Self {
-        Self {
-            workspaces_metadata: vec![],
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Default)]
 struct AppSecureConfig {
-    llm_api: String,
-}
-
-impl AppSecureConfig {
-    pub fn new() -> Self {
-        Self {
-            llm_api: "".to_string(),
-        }
-    }
+    llm_api: Option<String>,
 }
 
 // Main Parent State
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Default)]
+#[serde(default)]
 pub struct AppPersistentStates {
-    schema_version: u8,
     app_config: AppConfig,
     secure: AppSecureConfig,
 }
 
 impl AppPersistentStates {
-    pub fn new() -> Self {
-        Self {
-            schema_version: 1,
-            app_config: AppConfig::new(),
-            secure: AppSecureConfig::new(),
-        }
-    }
     pub fn save_states(
         &mut self,
         app: &AppHandle,
         states: &AppPersistentStates,
     ) -> Result<(), Box<dyn Error>> {
-        self.schema_version = states.schema_version;
         self.app_config = states.app_config.clone();
         self.secure = states.secure.clone();
 
@@ -94,27 +69,7 @@ impl AppPersistentStates {
         let path = app.path().app_config_dir().unwrap().join(CONFIG_FILE_NAME);
         let file = fs::File::options().read(true).open(path)?;
         *self = serde_json::from_reader(&file)?;
-
-        // Run migrations if needed
-        if self.schema_version == LATEST_SCHEMA_V {
-            return Ok(());
-        }
-
-        Err(format!(
-            "Current Schema version is Unsuppoirted : {}",
-            self.schema_version
-        )
-        .into())
-
-        // Future Migration Pipeline (Uncomment when LATEST_SCHEMA_V is 2)
-        // while self.schema_version < LATEST_SCHEMA_V {
-        //     match self.schema_version {
-        //         1 => migrate_1_to_2(&mut self),
-        //         _ => return Err("The Schema is not Valid".into()),
-        //     }
-        // }
-
-        // Ok(())
+        Ok(())
     }
     pub fn setup_config_path(&self, app: &AppHandle) -> Result<(), Box<dyn Error>> {
         let path = app.path().app_config_dir().unwrap().join(CONFIG_FILE_NAME);
